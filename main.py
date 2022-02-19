1d41c0eeef41b53a28a3df9eab9faeb6574bcb03
PuppoC <sperojohny@gmail.com>
2022-02-19 10:42:25 +0800

    20 fps

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,10)
+    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,20)
 
 if __name__== "__main__":
     main()
