8955f68e361fabfdaf9b12249fc3112b4f80431d
PuppoC <sperojohny@gmail.com>
2022-02-19 10:49:31 +0800

    even higher fpd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,20)
+    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.3,15)
 
 if __name__== "__main__":
     main()
